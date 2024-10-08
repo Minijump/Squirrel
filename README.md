bd8d72e0ee2ba9d1adceff0e127e518acf1a744a
SJ <sacha.jumpertz@gmail.com>
2024-10-08 14:56:30 +0200

    [MVP] enable multi-lines actions

--- README.md
+++ README.md
@@ -91,7 +91,7 @@ Create data analysis pipeline by generating python with a low-code interface.
 5. **Create a new Pull Request**
 
 ### To do MVP
-* Editable pipeline + pipeline of (multiline) actions and not only file's line
+* Editable pipeline
 * Solve perf issues (do not convert all df to html, start editing 'datatable', ...)
 * Try some more modular architecture
 * Enable to do wathever you want with python (Give column info (nulls, zeros, unique, ...), delete lines where -, replace col values, ...)
--- app/routers/pipeline.py
+++ app/routers/pipeline.py
@@ -13,12 +13,13 @@ def get_file_lines(file_path):
 
     * file_path(str): The path to the file
 
-    => Returns a list of lines; if the line is an action, it is represented as a tuple (action_id, line)
+    => Returns a list of lines; if the line(s) is an action, it is represented as a tuple (action_id, line)
     """
     lines = []
     with open(file_path, 'r') as file:
         in_pipeline = False
         action_id = 0
+        temp_action_lines = "" # Enables to deal with multi-line actions
         for line in file.readlines():
             if PIPELINE_START_TAG in line:
                 lines.append(line)
@@ -27,8 +28,14 @@ def get_file_lines(file_path):
                 lines.append(line)
                 in_pipeline = False
             elif in_pipeline and NEW_CODE_TAG not in line and line.strip() != "":
-                lines.append((action_id, line))
-                action_id += 1
+                if "#sq_action:" not in line:
+                    temp_action_lines += line
+                    continue
+                else:
+                    temp_action_lines += line
+                    lines.append((action_id, temp_action_lines))
+                    temp_action_lines = ""
+                    action_id += 1
             else:
                 lines.append(line)
     return lines
