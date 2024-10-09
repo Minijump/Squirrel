23837293c9d31d9d0f106aa34d15fd39f3f2c983
SJ <sacha.jumpertz@gmail.com>
2024-10-09 16:42:45 +0200

    [MVP] add pipeline tests

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -48,6 +48,10 @@ def run_pipeline():
     dfs['df'] = pd.DataFrame(data, columns=['name', 'price'])
 
     # Squirrel Pipeline start
+    dfs['df']['action1'] = 1  #sq_action: action1
+    #Useless comment, must have no effect (comment + 2 next python line should be 1 action)
+    dfs['df']['action2'] = 1
+    dfs['df']['action2'] = 2  #sq_action: action2
     # Add new code here (keep this comment line)
     # Squirrel Pipeline end
 
