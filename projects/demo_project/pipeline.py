f202cfaa7003e1433ef84f633c85b697e20d6c54
SJ <sacha.jumpertz@gmail.com>
2024-10-11 06:59:07 +0200

    [MVP] edit demo project + several fixes

--- projects/demo_project/pipeline.py
+++ projects/demo_project/pipeline.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 
 def run_pipeline():
-    df = False
+    dfs = {}
+
     # Squirrel Pipeline start
-    df = pd.read_csv(r'C:\Users\sacha\Documents\projects\Squirrel\projects\demo_project\data_sources\demo_data_source\data.csv')
-    df['test'] = 3
+    dfs['table1'] = pd.read_csv(r'C:\Users\sacha\Documents\projects\Squirrel\projects\demo_project\data_sources\demo_data_source\data.csv')  #sq_action:Create table table1 from demo_data_source
+    dfs['table1']['col_test'] = 1  #sq_action:Add column col_test on table table1
+    dfs['table1']['col_test2'] = 9  #sq_action:Add column col_test2 on table table1
+    dfs['table2'] = pd.read_csv(r'C:\Users\sacha\Documents\projects\Squirrel\projects\demo_project\data_sources\demo_data_source\data.csv')  #sq_action:Create table table2 from demo_data_source
+    dfs['table2']['heloooo'] = 88  #sq_action:Add column heloooo on table table2
+    dfs['table2'] = dfs['table2'].drop(columns=['price'])  #sq_action:Delete column price on table table2
+    dfs['table3'] = pd.read_csv(r'C:\Users\sacha\Documents\projects\Squirrel\projects\demo_project\data_sources\demo_data_source\data.csv')  #sq_action:Create table table3 from demo_data_source
     # Add new code here (keep this comment line)
     # Squirrel Pipeline end
 
-    # No edit under
-    return df
+    return dfs
