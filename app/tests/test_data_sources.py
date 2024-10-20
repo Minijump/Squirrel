2dd5141d167e598aa64bb3cce3afb3c162ad7c27
SJ <sacha.jumpertz@gmail.com>
2024-10-20 14:15:35 +0200

    [MVP] refactoring.1.3: add unit tests

--- app/tests/test_data_sources.py
+++ app/tests/test_data_sources.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 
 import pandas as pd
 import os
+import tempfile
 from unittest.mock import patch
 
 from app.main import app
@@ -24,3 +25,16 @@ def test_data_sources(mock_project):
         "directory": "mock_source_csv"
     }
     assert expected_source in response.context.get("sources"), "Expected one mock source csv"
+
+def test_fail_data_source_creation(mock_project):
+    """
+    Test if in case of a failing source creation, page is displayed correctly
+    """
+    form_data = {
+        "source_name": "Other arguments are missing=> will raise an error", 
+        }
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with patch('os.getcwd', return_value=temp_dir):
+            response = client.post("/create_source/", data=form_data)
+            assert response.status_code == 200, "Unexpected error while failing to create data source"
+            assert response.context.get("exception"), "Response does not contain an exception"
