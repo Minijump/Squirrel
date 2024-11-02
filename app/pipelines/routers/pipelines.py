491e99da120f6623428dd5f287467a094f1d4bcb
SJ <sacha.jumpertz@gmail.com>
2024-11-02 11:41:08 +0100

    [MVP] refactoring.2.6: imp pipeline routers

--- app/pipelines/routers/pipelines.py
+++ app/pipelines/routers/pipelines.py
@@ -2,12 +2,14 @@ from fastapi import Request
 from fastapi.responses import JSONResponse, RedirectResponse
 
 import os
+import traceback
 
 from app import router, templates
 from utils import PIPELINE_START_TAG, PIPELINE_END_TAG, NEW_CODE_TAG
 
+# TODO : Create models (wait for concrete usecases)
 
-def get_file_lines(file_path):
+async def get_file_lines(file_path):
     """
     Returns the lines contained in the file at file_path
 
@@ -40,7 +42,6 @@ def get_file_lines(file_path):
                 lines.append(line)
     return lines
 
-@router.post("/pipeline")
 @router.get("/pipeline")
 async def pipeline(request: Request, project_dir: str):
     """
@@ -52,25 +53,29 @@ async def pipeline(request: Request, project_dir: str):
     => Returns a TemplateResponse that displays the pipeline; 
     the actions are displayed with their squirrel name if they have one
     """
-    pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
-    lines = get_file_lines(pipeline_path)
+    try:
+        pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
+        lines = await get_file_lines(pipeline_path)
 
-    actions = []
-    for line in lines:
-        if isinstance(line, tuple):
-            if "#sq_action:" in line[1]:
-                actions.append((line[0], line[1].split("#sq_action:")[1].strip(), line[1]))
-            else:
-                actions.append(line)
-       
-    return templates.TemplateResponse(
-        request,
-        "pipeline.html",
-        {"actions": actions, "project_dir": project_dir}
-    )
+        actions = []
+        for line in lines:
+            if isinstance(line, tuple):
+                if "#sq_action:" in line[1]:
+                    actions.append((line[0], line[1].split("#sq_action:")[1].strip(), line[1]))
+                else:
+                    actions.append(line)
+        
+        return templates.TemplateResponse(
+            request,
+            "pipeline.html",
+            {"actions": actions, "project_dir": project_dir}
+        )
+    except Exception as e:
+        traceback.print_exc()
+        return templates.TemplateResponse(request, "tables_error.html", {"exception": str(e), "project_dir": project_dir})
+
 
 @router.post("/pipeline/delete_action")
-@router.get("/pipeline/delete_action")
 async def delete_action(request: Request, project_dir: str, delete_action_id: int):
     """
     Remove an action from the pipeline
@@ -81,23 +86,26 @@ async def delete_action(request: Request, project_dir: str, delete_action_id: in
 
     => Returns a JSONResponse if OK
     """
-    pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
-    lines = get_file_lines(pipeline_path)
+    try:
+        pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
+        lines = await get_file_lines(pipeline_path)
 
-    new_lines = []
-    for line in lines:
-        if not isinstance(line, tuple):
-            new_lines.append(line)
-        elif line[0] != delete_action_id:
-            new_lines.append(line[1])
+        new_lines = []
+        for line in lines:
+            if not isinstance(line, tuple):
+                new_lines.append(line)
+            elif line[0] != delete_action_id:
+                new_lines.append(line[1])
 
-    with open(pipeline_path, 'w') as file:
-        file.writelines(new_lines)
+        with open(pipeline_path, 'w') as file:
+            file.writelines(new_lines)
 
-    return JSONResponse(content={"message": "Action deleted successfully"}, status_code=200)
+        return JSONResponse(content={"message": "Action deleted successfully"}, status_code=200)
+    except Exception as e:
+        traceback.print_exc()
+        return templates.TemplateResponse(request, "tables_error.html", {"exception": str(e), "project_dir": project_dir})
 
 @router.post("/pipeline/confirm_new_order")
-@router.get("/pipeline/confirm_new_order")
 async def confirm_new_order(request: Request, project_dir: str, order: str):
     """
     Reorder the actions in the pipeline, edit the python file
@@ -108,29 +116,32 @@ async def confirm_new_order(request: Request, project_dir: str, order: str):
 
     => Returns a JSONResponse if OK
     """
-    pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
-    lines = get_file_lines(pipeline_path)
+    try:
+        pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
+        lines = await get_file_lines(pipeline_path)
 
-    new_order = [int(action_str[0]) for action_str in order.split(",")] # the old ids in the new order
-    old_actions = [line for line in lines if isinstance(line, tuple)]  # the actions in the old order
-    new_lines = []
-    action_id = 0
-    for line in lines:
-        if isinstance(line, tuple):
-            id_of_old_order = new_order[action_id]  # Action that had id_of_old_order must now have action_id id
-            new_line = old_actions[id_of_old_order][1]  # [1] because (action_id, line)
-            new_lines.append(new_line)
-            action_id += 1
-        else:
-            new_lines.append(line)
+        new_order = [int(action_str[0]) for action_str in order.split(",")] # the old ids in the new order
+        old_actions = [line for line in lines if isinstance(line, tuple)]  # the actions in the old order
+        new_lines = []
+        action_id = 0
+        for line in lines:
+            if isinstance(line, tuple):
+                id_of_old_order = new_order[action_id]  # Action that had id_of_old_order must now have action_id id
+                new_line = old_actions[id_of_old_order][1]  # [1] because (action_id, line)
+                new_lines.append(new_line)
+                action_id += 1
+            else:
+                new_lines.append(line)
 
-    with open(pipeline_path, 'w') as file:
-        file.writelines(new_lines)
+        with open(pipeline_path, 'w') as file:
+            file.writelines(new_lines)
 
-    return JSONResponse(content={"message": "Order changed successfully"}, status_code=200)
+        return JSONResponse(content={"message": "Order changed successfully"}, status_code=200)
+    except Exception as e:
+        traceback.print_exc()
+        return templates.TemplateResponse(request, "tables_error.html", {"exception": str(e), "project_dir": project_dir})
 
 @router.post("/pipeline/edit_action")
-@router.get("pipeline/edit_action")
 async def edit_action(request: Request):
     """
     Edit the code of an action in the pipeline
@@ -142,25 +153,29 @@ async def edit_action(request: Request):
 
     => Returns a redirect response to /pipeline
     """
-    form_data = await request.form()
-    action_id = int(form_data["action_id"])
-    action_code = form_data["action_code"]
-    project_dir = form_data["project_dir"]
+    try:
+        form_data = await request.form()
+        action_id = int(form_data["action_id"])
+        action_code = form_data["action_code"].replace("\r\n", "\n") # Temp fix (for windows?), avoid a new empty line
+        project_dir = form_data["project_dir"]
 
-    pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
-    lines = get_file_lines(pipeline_path)
+        pipeline_path = os.path.join(os.getcwd(), "_projects", project_dir, "pipeline.py")
+        lines = await get_file_lines(pipeline_path)
 
-    new_lines = []
-    for line in lines:
-        if isinstance(line, tuple):
-            if line[0] == action_id:
-                new_lines.append(action_code)
+        new_lines = []
+        for line in lines:
+            if isinstance(line, tuple):
+                if line[0] == action_id:
+                    new_lines.append(action_code)
+                else:
+                    new_lines.append(line[1])
             else:
-                new_lines.append(line[1])
-        else:
-            new_lines.append(line)
-    
-    with open(pipeline_path, 'w') as file:
-        file.writelines(new_lines)
+                new_lines.append(line)
+        
+        with open(pipeline_path, 'w') as file:
+            file.writelines(new_lines)
 
-    return RedirectResponse(url=f"/pipeline?project_dir={project_dir}")
+        return RedirectResponse(url=f"/pipeline?project_dir={project_dir}", status_code=303)
+    except Exception as e:
+        traceback.print_exc()
+        return templates.TemplateResponse(request, "tables_error.html", {"exception": str(e), "project_dir": project_dir})
