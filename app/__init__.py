b085659155eca2b5ee0d8db2573445d8c3976a83
SJ <sacha.jumpertz@gmail.com>
2024-11-23 15:43:36 +0100

    [MVP] refactoring 3.6: add pipeline templates

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,15 @@ logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 # TODO: group all static into one /projects, directory="projects" name='static) Need to import them in router too?????
-app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/templates/base/static", StaticFiles(directory="templates/base/static"), name="base_static")
 app.mount("/templates/projects/static", StaticFiles(directory="templates/projects/static"), name="projects_static")
 app.mount("/templates/data_sources/static", StaticFiles(directory="templates/data_sources/static"), name="data_sources_static")
 app.mount("/templates/tables/static", StaticFiles(directory="templates/tables/static"), name="tables_static")
+app.mount("/templates/pipeline/static", StaticFiles(directory="templates/pipeline/static"), name="pipeline_static")
 templates = Jinja2Templates(directory="templates")
 
 from fastapi import APIRouter
 router = APIRouter()
-router.mount("/../static", StaticFiles(directory="static"), name="static")
 app.mount("/templates/base/static", StaticFiles(directory="templates/base/static"), name="base_static")
 router.mount("/templates/projects/static", StaticFiles(directory="templates/projects/static"), name="projects_static")
 
