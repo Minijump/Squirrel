28ee4b14ed9ea6193993ea5b45f5d221f6c91d76
SJ <sacha.jumpertz@gmail.com>
2024-12-11 14:41:32 +0100

    [MVP] edit readme + edit demo project

--- _projects/demo_project/pipeline.py
+++ _projects/demo_project/pipeline.py
@@ -1,15 +1,17 @@
-import pandas as pd
 
+import pandas as pd
 
 def run_pipeline():
     dfs = {}
-
+    
     # Squirrel Pipeline start
-    dfs['table1'] = pd.read_csv(r'_projects\demo_project\data_sources\demo_data_source\data.csv')  #sq_action:Create table table1 from demo_data_source
-    dfs['table2'] = pd.read_csv(r'_projects\demo_project\data_sources\demo_data_source\data.csv')  #sq_action:Create table table2 from demo_data_source
-    dfs['table1']['col1'] = 9  #sq_action:Add column col1 on table table1
-    dfs['table2'] = dfs['table2'].drop(columns=['price'])  #sq_action:Delete column price on table table2
-    dfs['table2']['col2'] = 88  #sq_action:Add column col2 on table table2
+    dfs['Tech_stocks'] = pd.read_pickle(r'_projects\demo_project\data_sources\Stock_tech\data.pkl')  #sq_action:Create table Tech_stocks from Stock_tech
+    dfs['AU_stock'] = pd.read_pickle(r'_projects\demo_project\data_sources\Stock_AU\data.pkl')  #sq_action:Create table AU_stock from Stock_AU
+    dfs['Mosck_csv'] = pd.read_csv(r'_projects\demo_project\data_sources\Mock_CSV\data.csv')  #sq_action:Create table Mosck_csv from Mock CSV
+    dfs['pickle_file'] = pd.read_pickle(r'_projects\demo_project\data_sources\Mock_Pickle\data.pkl')  #sq_action:Create table pickle_file from Mock Pickle
+    dfs['pickle_file']['test'] = "test"  #sq_action:Add column test on table pickle_file
+    dfs['pickle_file']['test'] = dfs['pickle_file']['test'].astype('string')  #sq_action:Change type of column test to string in table pickle_file
+    dfs['pickle_file'] = dfs['pickle_file'].sort_values(by=['id'], ascending=True)  #sq_action:Sort(asc) id of table pickle_file
     # Add new code here (keep this comment line)
     # Squirrel Pipeline end
 
