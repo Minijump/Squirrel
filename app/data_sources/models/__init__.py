538a37a54e0305c5c36cd2eab8e9814718b55989
SJ <sacha.jumpertz@gmail.com>
2024-12-12 09:08:51 +0100

    [MVP] restructure ideas and tests

--- app/data_sources/models/__init__.py
+++ app/data_sources/models/__init__.py
@@ -1,4 +1,5 @@
 from .data_source import DataSource, DATA_SOURCE_REGISTRY
+from .data_source_file import DataSourceFile
 from .data_source_csv import DataSourceCSV
 from .data_source_xlsx import DataSourceXLSX
 from .data_source_odoo import DataSourceOdoo
