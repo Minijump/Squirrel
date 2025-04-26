d0627f945400853686979831d9fb69c546d1c591
SJ <sacha.jumpertz@gmail.com>
2025-04-26 13:23:45 +0200

    [MVP] refactor UT: add toolbox for project tours

--- tests/utils/tours_toolbox.py
+++ tests/utils/tours_toolbox.py
@@ -1,6 +1,8 @@
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BaseTool:
@@ -8,6 +10,7 @@ class BaseTool:
     
     def __init__(self, browser: WebDriver):
         self.browser = browser
+
 
 class App(BaseTool):
     """Represents the main application."""
@@ -20,6 +23,47 @@ class App(BaseTool):
         if url:
             actual_url = self.browser.current_url
             assert url in actual_url, f"Expected '{url}' in the url"
+
+    def check_elements(self, by_ids: list = False, by_css_selectors: list = False) -> None:
+        """
+        Check if the elements with the given selector are visible on the page and have the expected values.
+        
+        :param by_ids: selector based on ids; [(id, expected_value)]
+        :param by_css_selectors: selector based on css selectors; [(css_selector, expected_value)]
+        """
+        by_ids = by_ids or []
+        by_css_selectors = by_css_selectors or []
+
+        def check_element(element):
+            assert element.is_displayed(), f"Element is not displayed."
+            actual_value = element.get_attribute("value")
+            assert actual_value == expected_value, f"Expected value: {expected_value}, but got: {actual_value}"
+
+        for id, expected_value in by_ids:
+            element = self.browser.find_element(By.ID, id)
+            check_element(element)
+
+        for css_selector, expected_value in by_css_selectors:
+            element = self.browser.find_element(By.CSS_SELECTOR, css_selector)
+            check_element(element)
+
+    def fill_element(self, by_id: str = False, by_css_selector: str = False, value: str = False) -> None:
+        """Fill an element with the given value."""
+        if by_id:
+            element = self.browser.find_element(By.ID, by_id)
+        elif by_css_selector:
+            element = self.browser.find_element(By.CSS_SELECTOR, by_css_selector)
+        else:
+            raise ValueError("Either 'by_id' or 'by_css_selector' must be provided.")
+
+        element.click()
+        element.clear()
+        element.send_keys(value)
+
+    def assert_error_page(self) -> None:
+        """Check if the error page is displayed."""
+        assert self.browser.find_element(By.CSS_SELECTOR, "h3").text == "Error", "Error page not displayed as expected."
+
 
 class Navbar(BaseTool):
     """Represents the navigation bar of the application."""
@@ -43,9 +87,71 @@ class Navbar(BaseTool):
             if hover_color == original_color:
                 assert False, f"Hover effect not working on link with text: {link.text}"
 
-class Tour(App, Navbar):
+
+class Modal(BaseTool):
+    """Represents a modal dialog in the application."""
+
+    def __init__(self, browser, expected_visible) -> None:
+        super().__init__(browser)
+        self.expected_visible = expected_visible
+        self.assert_visibility(visible=True)
+        
+    def assert_visibility(self, visible=True) -> None:
+        if visible:
+            WebDriverWait(self.browser, 2, poll_frequency=0.1).until(
+                expected_conditions.visibility_of_element_located((By.XPATH, self.expected_visible)),
+                message="Modal did not appear as expected.")
+        else:
+            WebDriverWait(self.browser, 10, poll_frequency=0.1).until(
+                expected_conditions.invisibility_of_element_located((By.XPATH, self.expected_visible)),
+                message="Modal did not close as expected.")
+            
+    def fill(self, field_id: str, value: str) -> None:
+        """Fill a field in the modal dialog."""
+        field = self.browser.find_element(By.ID, field_id)
+        field.click()
+        field.clear()
+        field.send_keys(value)
+
+    def close(self, assert_closed=True) -> None:
+        """Close the modal dialog."""
+        self.browser.find_element(By.ID, "cancelButton").click()
+        self.assert_visibility(visible=not assert_closed)
+        
+    def submit(self, assert_closed=True) -> None:
+        """Submit the modal dialog."""
+        self.browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        self.assert_visibility(visible=not assert_closed)
+
+
+class Grid(BaseTool):
+    """Represents a grid view in the application."""
+
+    def click_create_card(self, expected_visible=False) -> Modal:
+        """Click on the create card button."""
+        self.browser.find_element(By.CSS_SELECTOR, "p:nth-child(1)").click()
+        return Modal(self.browser, expected_visible)
+    
+    def click_card(self, by_xpath:str = False, by_title:str = False) -> None:
+        """Click on a card in the grid."""
+        if by_xpath:
+            self.browser.find_element(By.XPATH, by_xpath).click()
+        elif by_title:
+            xpath = f"//div[@class=\'grid\']//h3[text()=\'{by_title}\']"
+            self.browser.find_element(By.XPATH, xpath).click()
+
+
+class Tour(App, Navbar, Grid):
     """Represents a tour."""
 
     def __init__(self, browser: WebDriver, tour_name: str = "Tour") -> None:
         super().__init__(browser)
         self.tour_name = tour_name
+
+    def create_project(self, name, description=False):
+        """Create a new project with the given name and description."""
+        create_project_modal = self.click_create_card(expected_visible="//form[@id=\'projectForm\']")
+        create_project_modal.fill("projectName", name)
+        if description:
+            create_project_modal.fill("projectDescription", description)
+        create_project_modal.submit(assert_closed=True)
