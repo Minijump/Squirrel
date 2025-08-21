12823a81b785f25449cae8d78818f8e688ffd0a6
SJ <sacha.jumpertz@gmail.com>
2025-08-21 14:40:00 +0200

    [MVP] Refactor: form_utils refactor

--- app/utils/form_utils.py
+++ app/utils/form_utils.py
@@ -10,12 +10,8 @@ from app import templates
 
 app = FastAPI()
 
-# Used in Unit Tests
-SQUIRREL_ERROR_DECORATED = set()
-SQUIRREL_ACTION_ERROR_DECORATED = set()
 
 def squirrel_error(func):
-    SQUIRREL_ERROR_DECORATED.add(func.__name__)
     @wraps(func)
     async def wrapper(request: Request, *args, **kwargs):
         try:
@@ -24,20 +20,12 @@ def squirrel_error(func):
             traceback.print_exc()
             form_data = await request.form()
             project_dir = form_data.get("project_dir")
-            if project_dir:
-                return templates.TemplateResponse(
-                    request, 
-                    "utils/templates/tables_error.html",
-                    {"exception": str(e), "project_dir": project_dir})
-            else:
-                return templates.TemplateResponse(
-                    request, 
-                    "utils/templates/projects_error.html", 
-                    {"exception": str(e)})
+            template_path = "utils/templates/tables_error.html" if project_dir else "utils/templates/projects_error.html"
+            additional_context = {"exception": str(e), "project_dir": project_dir} if project_dir else {"exception": str(e)}
+            return templates.TemplateResponse(request, template_path, additional_context)
     return wrapper
 
 def squirrel_action_error(func):
-    SQUIRREL_ACTION_ERROR_DECORATED.add(func.__name__)
     @wraps(func)
     async def wrapper(request: Request, *args, **kwargs):
         try:
@@ -46,15 +34,9 @@ def squirrel_action_error(func):
             traceback.print_exc()
             form_data = await request.form()
             project_dir = form_data.get("project_dir")
-            if project_dir:
-                error_message = str(e)
-                notification_param = quote(error_message)
-                return RedirectResponse(url=f"/tables/?project_dir={project_dir}&notification={notification_param}", status_code=303)
-            else:
-                return templates.TemplateResponse(
-                    request, 
-                    "utils/templates/projects_error.html", 
-                    {"exception": str(e)})
+            notification_param = quote(str(e))
+            url = f"/tables/?project_dir={project_dir}&notification={notification_param}"
+            return RedirectResponse(url=url, status_code=303)
     return wrapper
 
 async def _get_form_data_info(request: Request, args_list: list):
