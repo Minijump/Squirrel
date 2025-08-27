fb95b5ef9b5dac71df4377adb762fc195c0c22ce
SJ <sacha.jumpertz@gmail.com>
2025-08-27 15:27:13 +0200

    [MVP] DataSource refactor: test DataSourceFile

--- tests/test_data_sources_file_class.py
+++ tests/test_data_sources_file_class.py
@@ -1,5 +1,3 @@
-import pytest
-
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -9,24 +7,7 @@ from app.data_sources.models.data_source_file import DataSourceFile
 client = TestClient(app)
 
 
-def test_instance_from_manifest():
-    """
-    Test instance creation of DataSourceFile from manifest
-    """
-    manifest = {
-        "name": "Mock source",
-        "directory": "mock_directory",
-        "project_dir": "mock_project",
-        "type": "csv",
-        "kwargs": {}
-    }
-    source = DataSourceFile(manifest)
-    assert source.name == "Mock source"
-
 def test_create_table():
-    """
-    Test the create_table method of DataSourceFile
-    """
     form_data = {
         "name": 'mock_source',
         "type": 'csv',
@@ -35,9 +16,7 @@ def test_create_table():
         "project_dir": "mock_project"
     }
     source = DataSourceFile(form_data)
-    table_form_data = {
-        "project_dir": "mock_fake_dir",
-        "table_name": "mock_table",
-    }
-    python_line =  source.create_table(table_form_data)
+
+    python_line =  source.create_table({"table_name": "mock_table"})
+
     assert " pd.read_pickle(r" in python_line, f"DataSourceFile create table method should read the pickle file"
