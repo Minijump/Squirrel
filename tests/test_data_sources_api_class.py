7f06fd98ec7edc4530cfdafb83dcd87c531aac00
SJ <sacha.jumpertz@gmail.com>
2025-08-27 15:30:21 +0200

    [MVP] DataSource refactor: test API sources

--- tests/test_data_sources_api_class.py
+++ tests/test_data_sources_api_class.py
@@ -5,37 +5,15 @@ from app.data_sources.models.data_source_api import DataSourceAPI
 
 client = TestClient(app)
 
-
-def test_instance_from_manifest():
-    """
-    Test instance creation of DataSourceAPI from manifest (should include last_sync)
-    """
-    manifest = {
-        "name": "Mock source",
-        "directory": "mock_directory",
-        "type": "API",
-        "last_sync": "2021-01-01 00:00:00",
-        "project_dir": "mock_project"
-    }
-    source = DataSourceAPI(manifest)
-    assert source.name == "Mock source"
-    assert source.last_sync == "2021-01-01 00:00:00"
-
 def test_create_table():
-    """
-    Test the create_table method of DataSourceFile
-    """
     form_data = {
         "name": 'mock_source',
-        "type": 'csv',
-        "kwargs": '{"delimiter": ";"}',
+        "type": 'api (mock)',
         "directory": 'mock_directory',
         "project_dir": "mock_project"
     }
     source = DataSourceAPI(form_data)
-    table_form_data = {
-        "project_dir": "mock_fake_dir",
-        "table_name": "mock_table",
-    }
-    python_line =  source.create_table(table_form_data)
+
+    python_line =  source.create_table({"table_name": "mock_table"})
+
     assert " pd.read_pickle(r" in python_line, f"DataSourceAPI create table method should read the pickle file"
