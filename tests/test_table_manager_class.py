d093738e1abe7a486dad8aee74bd5a76b1f3a533
SJ <sacha.jumpertz@gmail.com>
2025-08-30 15:38:14 +0200

    [MVP] add table manager tests

---
+++ tests/test_table_manager_class.py
@@ -0,0 +1,53 @@
+import pytest
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.tables.models.table_manager import TableManager
+from tests import MOCK_PROJECT
+
+
+client = TestClient(app)
+
+
+@pytest.mark.asyncio
+async def test_init_from_project_dir_not_lazy(temp_project_dir_fixture):
+    project_dir = MOCK_PROJECT
+    
+    table_manager = await TableManager.init_from_project_dir(project_dir, lazy=False)
+
+    assert isinstance(table_manager, TableManager)
+
+@pytest.mark.asyncio
+async def test_init_from_project_dir_lazy(temp_project_dir_fixture):
+    project_dir = MOCK_PROJECT
+    table_manager_pipeline_run = await TableManager.init_from_project_dir(project_dir, lazy=False)
+
+    table_manager_file = await TableManager.init_from_project_dir(project_dir, lazy=True)
+
+    assert set(table_manager_file.tables.keys()) == set(table_manager_pipeline_run.tables.keys())
+
+@pytest.mark.asyncio
+async def test_to_html_default(temp_project_dir_fixture):
+    project_dir = MOCK_PROJECT
+    table_manager = await TableManager.init_from_project_dir(project_dir, lazy=False)
+    
+    table_html, table_len_infos = table_manager.to_html()
+    
+    assert isinstance(table_html, dict)
+    assert isinstance(table_len_infos, dict)
+    assert "ordered" in table_html
+
+@pytest.mark.asyncio
+async def test_get_col_info(temp_project_dir_fixture):
+    project_dir = MOCK_PROJECT
+    table_manager = await TableManager.init_from_project_dir(project_dir, lazy=False)
+    
+    col_info = table_manager.get_col_info("ordered", "mock_name")
+    
+    assert isinstance(col_info, dict)
+    assert "dtype" in col_info
+    assert "unique" in col_info
+    assert "null" in col_info
+    assert "count" in col_info
+    assert "is_numeric" in col_info
